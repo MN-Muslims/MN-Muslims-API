61c0bae62c84feaac927eaf12fa161435890c687
N00rAhmed <tronn232003@gmail.com>
2023-06-28 18:29:14 +0100

    freifo

--- controllers/adminUserController.js
+++ controllers/adminUserController.js
@@ -44,13 +44,12 @@ const UserController = {
       // }
 
       // Hash the password
-      // const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(password, 10);
 
       // Create a new user
       const newUser = new User({
         username,
-        password
-        // password: hashedPassword,
+        password: hashedPassword,
       });
 
       // Save the user to the database
